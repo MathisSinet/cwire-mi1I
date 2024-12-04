7c0d308d91a8274e567196a5c45bd9f217597da3
Mathis Sinet <mathis.sinet@gmail.com>
2024-12-04 20:48:04 +0100

    Ajout vérif options dans script shell

--- c-wire.sh
+++ c-wire.sh
@@ -7,7 +7,60 @@ do
     if [ $arg = '-h' ]
     then
         ./help.sh
-        exit
+        exit 1
     fi
 done
+
+# Vérification des arguments
+
+if (($# < 3))
+then
+    echo "Nombre d'arguments invalide"
+    echo "Utilisez c-wire.sh -h pour obtenir de l'aide"
+    exit 2
+fi
+
+if [ ! -f $1 ] || [ ! -r $1 ]
+then
+    echo "$1 n'est pas un fichier valide"
+    echo "Utilisez c-wire.sh -h pour obtenir de l'aide"
+    exit 3
+else
+    chemin_entree=$1
+fi
+
+type_station=-1
+case $2 in
+    'hva') type_station=0 
+    ;;
+    'hvb') type_station=1 
+    ;;
+    'lv') type_station=2
+    ;;
+    *)
+        echo "Type de station inconnu '$2'"
+        echo "Utilisez c-wire.sh -h pour obtenir de l'aide"
+        exit 4
+        ;;
+esac
+
+type_conso=-1
+case $3 in
+    'comp') type_conso=0 
+    ;;
+    'indiv') type_conso=1
+    if (($type_station != 2))
+    then
+        echo "Impossible d'utiliser l'option 'indiv' avec une station $2"
+        exit 5
+    fi
+    ;;
+    *)
+        echo "Type de consommateur inconnu '$3'"
+        echo "Utilisez c-wire.sh -h pour obtenir de l'aide"
+        exit 6
+        ;;
+esac
+
+id_centrale=-1
 
--- help.sh
+++ help.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Script pour afficher l'aide de c-wire.sh
 
 echo "Aide pour C-wire"
 echo "À faire"
