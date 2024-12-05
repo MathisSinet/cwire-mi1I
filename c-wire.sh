9f9106ef9d3157d2b52a202265c0d32b5bae68ab
Mathis Sinet <mathis.sinet@gmail.com>
2024-12-05 08:32:01 +0100

    màj script shell

--- c-wire.sh
+++ c-wire.sh
@@ -1,34 +1,40 @@
 #!/bin/bash
 
-# Vérification de la présence de l'option -h
+# Fonction d'erreur
+erreur()
+{
+    ./help.sh
+    echo "Temps : 0.00 secondes"
+    exit $1
+}
 
+# Vérification de la présence de l'option -h
 for arg in $@
 do
     if [ $arg = '-h' ]
     then
         ./help.sh
-        exit 1
+        exit 0
     fi
 done
 
-# Vérification des arguments
-
+# Vérification du nombre d'arguments
 if (($# < 3))
 then
-    echo "Nombre d'arguments invalide"
-    echo "Utilisez c-wire.sh -h pour obtenir de l'aide"
-    exit 2
+    echo "Erreur : Nombre d'arguments invalide"
+    erreur 2
 fi
 
+# Vérification de l'argument 1 "fichier d'entrée"
 if [ ! -f $1 ] || [ ! -r $1 ]
 then
-    echo "$1 n'est pas un fichier valide"
-    echo "Utilisez c-wire.sh -h pour obtenir de l'aide"
-    exit 3
+    echo "Erreur : '$1' n'est pas un fichier valide"
+    erreur 3
 else
     chemin_entree=$1
 fi
 
+# Vérification de l'argument 2 "type de station"
 type_station=-1
 case $2 in
     'hva') type_station=0 
@@ -38,29 +44,34 @@ case $2 in
     'lv') type_station=2
     ;;
     *)
-        echo "Type de station inconnu '$2'"
-        echo "Utilisez c-wire.sh -h pour obtenir de l'aide"
-        exit 4
+        echo "Erreur : Type de station inconnu '$2'"
+        erreur 4
         ;;
 esac
 
+# Vérification de l'argument 3 "type de consommateur"
 type_conso=-1
 case $3 in
-    'comp') type_conso=0 
-    ;;
+    'comp') type_conso=0 ;;
     'indiv') type_conso=1
     if (($type_station != 2))
     then
-        echo "Impossible d'utiliser l'option 'indiv' avec une station $2"
-        exit 5
+        echo "Erreur : Impossible d'utiliser l'option 'indiv' avec une station $2"
+        erreur 5
     fi
     ;;
+    'all') type_conso=2 ;;
     *)
         echo "Type de consommateur inconnu '$3'"
-        echo "Utilisez c-wire.sh -h pour obtenir de l'aide"
-        exit 6
+        echo "Erreur : Utilisez c-wire.sh -h pour obtenir de l'aide"
+        erreur 6
         ;;
 esac
 
+# Vérification de l'argument 4 "identifiant de station"
 id_centrale=-1
+if (($# >= 4))
+then
+    id_centrale=$4
+fi
 
