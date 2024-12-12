76de52c6b2abddf4b9fe7133c765d9bdd34a034e
p0upoune <poupoune07@gmail.com>
2024-12-12 09:20:43 +0100

    change fopen with stdin and stdout

--- codeC/main.c
+++ codeC/main.c
@@ -2,8 +2,8 @@
 #include "avl.h"
 
 int main (int argc, char* argv[]) {
-    FILE* fichierEntree = fopen(argv[1], "r");
-    FILE* fichierSortie = fopen(argv[2], "w");
+    FILE* fichierEntree = stdin;
+    FILE* fichierSortie = stdout;
     int nbLignes = atoi(argv[3]);
     Station s;
     AVL* a = NULL;
@@ -13,13 +13,10 @@ int main (int argc, char* argv[]) {
     int id = 0;
 
     for (int i=0; i<nbLignes; i++) {
-        fscanf(fichierEntree, "%d %ld %ld", &id, &capacity, &load);
+        fscanf(fichierEntree, "%d;%ld;%ld", &id, &capacity, &load);
         a = insertionAVL(a, id, capacity, load, &h); 
     }
     afficheInfixe(a, fichierSortie);
-
-    fclose(fichierEntree);
-    fclose(fichierSortie);
     
     return 0;
 }
